45d6c33f293e5e1cb16ac1068ebd743b08206b15
MaximoMephisto <sorondomaximo1@gmail.com>
2021-09-22 23:57:52 -0300

    Repository

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MM | Portfolio</title>
+    <link rel="stylesheet" href="css/diseno.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/ff041f84fb.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+</head>
+<body id="body">
+    <div id="particles-js"></div>
+    <div class="container-particles">
+        <header>
+            <nav class="navbar fixed-top">
+                <div class="container-fluid">
+                    <a class="navbar-title">Portfolio</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                    <i class="fas fa-ellipsis-v"></i>
+                    </button>
+                    <div class="offcanvas offcanvas-end" 
+                        tabindex="-1" 
+                        id="offcanvasNavbar" 
+                        aria-labelledby="offcanvasNavbarLabel"
+                        >
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Documents</h5>
+                            <i class="fas fa-times" 
+                                type="button" 
+                                class="btn-close text-reset" 
+                                data-bs-dismiss="offcanvas" 
+                                aria-label="Close"
+                                >
+                            </i>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Contact</a>
+                                </li>
+                            </ul>
+                            <div class="col-12 col-sm-12 text-center mt-5 icons">
+                                <i class="fab fa-facebook-f"></i>
+                                <i class="fab fa-instagram"></i>
+                                <i class="fab fa-linkedin-in"></i>
+                                <i class="fab fa-telegram-plane"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm 12" id="welcome">
+                        <h1> 
+                            <span class="letters-one">¡</span><span class="letters-one">H</span><span class="letters-one">e</span><span class="letters-one">l</span><span class="letters-one">l</span><span class="letters-one">o</span><span class="letters-one">!</span>
+                            <br> 
+                            <span class="letters-two">I</span><span class="letters-two">'</span><span class="letters-two">m</span> <span class="special-letter">M</span><span class="letters-two">á</span><span class="letters-two">x</span><span class="letters-two">i</span><span class="letters-two">m</span><span class="letters-two">o</span><span class="letters-two">,</span>
+                            <br>
+                            <span class="letters-tree">a</span> <span class="letters-tree">W</span><span class="letters-tree">e</span><span class="letters-tree">b</span> <span class="letters-tree">D</span><span class="letters-tree">e</span><span class="letters-tree">v</span><span class="letters-tree">e</span><span class="letters-tree">l</span><span class="letters-tree">o</span><span class="letters-tree">p</span><span class="letters-tree">e</span><span class="letters-tree">r</span>
+                            <br>
+                            <p class="disabled-p">Programmer Web Full Stack / Mobile apps developer</p>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+
+        </main>
+    </div>
+    <script src="js/particles.js"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
