c557b0346261be9fa4deed8cf2da3626a60ea15d
MaximoMephisto <sorondomaximo1@gmail.com>
2022-01-24 14:09:28 -0300

    cambios

---
+++ js/sendData.js
@@ -0,0 +1,24 @@
+function sendData() {
+
+    var correo = new FormData();
+
+    correo.append("txtNombre", document.getElementById("txtNombre").value);
+    correo.append("txtMail", document.getElementById("txtMail").value);
+    correo.append("txtNombre", document.getElementById("txtNombre").value);
+    correo.append("txtReason", document.getElementById("txtReason").value);
+    correo.append("txtMessage", document.getElementById("txtMessage").value);
+
+    $.ajax({
+        url:"sendData.php",
+        type: "POST",
+        data: correo,
+        cache: false,
+        contentType: false,
+        processData: false,
+        msj=JSON.parse(resp),
+        success: function(resp){
+            alert(msj.mensaje);
+        }
+    })
+
+}
